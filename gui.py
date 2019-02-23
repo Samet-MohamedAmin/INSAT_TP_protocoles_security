8e340c8a8a5821897813922e9f0c3a42fcae309b
mohamedamin <mohamedamin.samet@gmail.com>
2019-02-23 13:33:51 +0100

    refactor code: load files in Source class

--- gui.py
+++ gui.py
@@ -13,12 +13,11 @@ SEP = ';'
 END = '.'
 
 class HandlerEvents:
-    def __init__(self, builder):
+    def __init__(self, builder, source=Source()):
         self.builder = builder
         self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
         self.result_buffer = self.builder.get_object('result_text').get_buffer()
-        self.text_src = ''
-        self.char_mapping = ''
+        self.source = source
 
     def open_about(self, widget):
         about_dialog = self.builder.get_object('about_dialog')
@@ -50,13 +49,15 @@ class HandlerEvents:
 
     def open_text_src(self, *args):
         text = self.open_dlg('open text source')
-        if text: self.update_input(input_id='input_text_src', text=text)
-        self.text_src_updated(text)
+        if text:
+            self.update_input(input_id='input_text_src', text=text)
+            self.text_src_updated(text)
 
     def open_char_mapping(self, *args):
         text = self.open_dlg('open char mapping')
-        if text: self.update_input(input_id='input_char_mapping', text=text)
-        self.char_mapping_updated(text)
+        if text:
+            self.update_input(input_id='input_char_mapping', text=text)
+            self.char_mapping_updated()
 
     def paste_text_src(self, widget):
         text = self.clipboard.wait_for_text()
@@ -66,42 +67,36 @@ class HandlerEvents:
     def paste_char_mapping(self, widget):
         text = self.clipboard.wait_for_text()
         self.update_input(input_id='input_char_mapping', text=text)
-        self.char_mapping_updated(text)
+        self.char_mapping_updated()
 
-    def text_src_updated(self, text):
+    def text_src_updated(self):
         text_src_image = builder.get_object('result_src_image')
-        if text:
-            text_src_image.set_from_icon_name('gtk-yes', Gtk.IconSize.MENU)
-            self.text_src = text
-            self.update_result()
-        else:
-            text_src_image.set_from_icon_name('gtk-no', Gtk.IconSize.MENU)
+        text_src_image.set_from_icon_name('gtk-yes', Gtk.IconSize.MENU)
+        self.update_result()
 
-    def char_mapping_updated(self, text):
+    def char_mapping_updated(self):
         char_mapping_image = builder.get_object('result_mapping_image')
-        if text:
+        if self.source.char_mapping:
             char_mapping_image.set_from_icon_name('gtk-yes', Gtk.IconSize.MENU)
-            self.char_mapping = text
             self.update_result()
         else:
             char_mapping_image.set_from_icon_name('gtk-no', Gtk.IconSize.MENU)
 
+    def params_vegenaire_changed(self, widget):
+        print(widget.get_text())
 
     def update_result(self, *widget):
-        source = Source()
-        print(self.char_mapping)
-        source.mapping_brut = json.loads(self.char_mapping)
-        source.original = self.text_src
-        source.normalize()
-        source.extract_words()
-        source.print_words()
-        crypt = Crypting(source)
-        crypt.cesar_decrypt()
-        source.extract_words()
-        source.print_words()
-
-    # but_image.set_from_icon_name('gtk-yes', Gtk.IconSize.MENU)
-
+        print('update result')
+        print(self.source.original)
+        print('_'*20)
+        if self.source.original:
+            self.source.normalize()
+            self.source.extract_words()
+            self.source.print_words()
+            crypt = Crypting(self.source)
+            crypt.cesar_decrypt()
+            # self.source.extract_words()
+            # self.source.print_words()
 
 
 if __name__ == "__main__":
@@ -111,28 +106,27 @@ if __name__ == "__main__":
     main_window = builder.get_object('main_window')
     main_window.show_all()
 
+    # create Source() object
+    src = Source(path_mapping=os.path.join('source', 'char_mapping_default.json'),
+                 path_src=os.path.join('source', 'chiffrage_source.txt'))
+
     # load handler events
-    handler = HandlerEvents(builder)
+    handler = HandlerEvents(builder=builder, source=src)
     builder.connect_signals(handler)
 
     # load default char mapping
-    with open(os.path.join('source', 'char_mapping_default.json')) as json_data:
-        char_mapping = json_data.read()
-    handler.char_mapping = char_mapping
-    builder.get_object('input_char_mapping').get_buffer().set_text(char_mapping)
-    builder.get_object('result_mapping_image')\
-        .set_from_icon_name('gtk-yes', Gtk.IconSize.MENU)
+    handler.update_input(input_id='input_char_mapping',
+                         text=src.load_char_mapping_from_file())
+    handler.char_mapping_updated()
 
     # load default text source
-    with open(os.path.join('source','chiffrage_source.txt')) as file:
-        text_src = file.read()
-    handler.text_src = text_src
-    builder.get_object('input_text_src').get_buffer().set_text(text_src)
-    builder.get_object('result_src_image')\
-        .set_from_icon_name('gtk-yes', Gtk.IconSize.MENU)
+    handler.update_input(input_id='input_text_src',
+                         text=src.load_text_src_from_file())
+    handler.text_src_updated()
+
 
     # test crypting
-    handler.update_result()
+    # handler.update_result()
 
     # connect to destroy event
     main_window.connect("destroy", Gtk.main_quit)
--- source/source.py
+++ source/source.py
@@ -1,29 +1,38 @@
 #! /bin/python3 
 
-import re, os
+import re, os, json
 
 
 class Source:
-    def __init__(self, file_name = 'chiffrage_source.txt'):
-        self.file_name = os.path.join('source', file_name)
+    def __init__(self,
+                 path_mapping='char_mapping_default.json',
+                 path_src='chiffrage_source.txt'):
+        # self.file_name = os.path.join('source', file_name)
         self.original = ''
         self.formatted = ''
         self.words = []
-        self.mapping_brut = {'à': 'a', 'é': 'e', 'è': 'e', 'ç': 'c'}
-
+        self.char_mapping = {} #{'à': 'a', 'é': 'e', 'è': 'e', 'ç': 'c'}
+        self.path_mapping = path_mapping
+        self.path_src = path_src
 
         # self.read_from_file()
         # self.normalize()
 
-    def read_from_file(self):
-        with open(self.file_name, 'r', encoding='utf-8') as file:
+    def load_char_mapping_from_file(self):
+        with open(self.path_mapping, 'r', encoding='utf-8') as file:
+            char_mapping_text = file.read()
+        self.char_mapping = json.loads(char_mapping_text)
+        return char_mapping_text
+
+    def load_text_src_from_file(self):
+        with open(self.path_src, 'r', encoding='utf-8') as file:
             self.original = file.read()
+        return self.original
 
     def normalize(self):
-
         # mapping = [('à', 'a'), ('é', 'e'), ('è', 'e'), ('ç', 'c')]
         # mapping = {ord('à'): ord('a'), ord('é'): ord('e'), ord('è'): ord('e')}
-        mapping = {ord(x): ord(y) for x, y in self.mapping_brut.items()}
+        mapping = {ord(x): ord(y) for x, y in self.char_mapping.items()}
 
         self.formatted = self.original.lower().translate(mapping)
 
@@ -44,8 +53,4 @@ class Source:
     def reorder(self):
         self.formatted = ' '.join(self.words)
 
-
-
-result = ''
-key = 'chiffrage'
 
